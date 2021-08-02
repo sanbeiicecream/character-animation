3b4bee37e267ab5ac9394eceb311f5f47aeff7be
miao <miao@163.com>
2021-08-02 08:08:03 +0800

    不太完美的实现

--- main.js
+++ main.js
@@ -1,64 +1,71 @@
-function get(id) {
-  return document.getElementById(id)
-}
-
-const img_arr = []
-const img_num = 34109
-const img = new Image()
-let info = get('info')
-img.src = 'assets/images/00001.jpg'
-img.onload = start
-
-function start() {
+const myVideo = document.querySelector('#myVideo')
+let videoHeight
+let videoWidth
+let textDiv = document.querySelector('#text')
+let contextWord = ['东','方','幻','想','乡']
+myVideo.ontimeupdate = function () {
+  videoHeight = myVideo.clientHeight
+  videoWidth = myVideo.clientWidth
   init()
-  if (img_arr.length === img_num) {
-    info.style.display = 'none'
-    textPlayer()//动画播放器播放
-  }
 }
 
 //转换
 function init() {
-  let cv = get('myCanvas')
-  let cxt = cv.getContext('2d')
+  let cv = document.querySelector('#myCanvas')
+  let ctx = cv.getContext('2d')
   //图片的高宽度给canvas画布
-  cv.width = img.width
-  cv.heigth = img.height
-  cxt.drawImage(img, 0, 0)//画出图像
-  let pic_data = cxt.getImageData(0, 0, img.width, img.height)
-  let pic_Data = pic_data.data//获取图片的data
+  cv.width = videoWidth
+  cv.height = videoHeight
+  ctx.drawImage(myVideo, 0, 0, videoWidth, videoHeight)//画出图像
+  let imageData = ctx.getImageData(0, 0, videoWidth, videoHeight).data//获取图片的data
+  ctx.clearRect(0, 0, videoWidth, videoHeight)
+  let content = ''
+  cv.font = '9px'
   //获取图片的高宽
-  let pic_DataWidth = pic_data.width
-  let pic_DataHeight = pic_data.height
+  // let imageWidth = ctx.getImageData(0, 0, videoWidth, videoHeight).width
+  // let imageHeight = ctx.getImageData(0, 0, videoWidth, videoHeight).height
   // 灰阶
-  let cont = ''
-  for (let h = 0; h < pic_DataHeight; h += 4) {
-    let line = ''
-    for (let w = 0; w < pic_DataWidth; w += 2) {
-      let index = (w + h * pic_DataWidth) * 4
-      let r = pic_Data[index]
-      let g = pic_Data[index + 1]
-      let b = pic_Data[index + 2]
+  for (let h = 0; h < videoHeight; h += 12) {
+    let line = '<div>'
+    for (let w = 0; w < videoWidth; w += 12) {
+      let index = (w + h * videoWidth) * 4
+      let r = imageData[index]
+      let g = imageData[index + 1]
+      let b = imageData[index + 2]
       let gray = getGray(r, g, b)
       line += toText(gray)
+      // let i = Math.min(contextWord.length - 1,Math.floor(gray / (255 / contextWord.length)))
+      // ctx.fillText(toText(gray), w, h)
     }
-    line += '</br>;'
-    cont += line
+    line += '</div>'
+    content += line
   }
-  img_arr[img_arr.length] = cont
-  info.innerHTML = '正在加载' + img.src
-  if (img_arr.length < img_num) {
-    img.scr = 'assets/images/' + imgName(img_arr.length) + '.jpg'
-  }
+  textDiv.innerHTML = content
 }
 
 //计算灰度值
 function getGray(r, g, b) {
+  // return (r * 30 + g * 59 + b * 11 + 50) / 100
   return 0.299 * r + 0.578 * g + 0.114 * b
 }
 
 //根据灰度值生成对应的字符
 function toText(g) {
+  // if (g <= 30) {
+  //   return '东'
+  // } else if (g <= 60) {
+  //   return '方'
+  // } else if (g <= 120) {
+  //   return '幻'
+  // } else if (g <= 150) {
+  //   return '像'
+  // } else if (g <= 180) {
+  //   return '乡'
+  // } else if (g <= 210) {
+  //   return '妙'
+  // } else if (g <= 240) {
+  //   return '呀'
+  // } else return ' '
   if (g <= 30) {
     return 'M'
   } else if (g <= 60) {
@@ -73,28 +80,5 @@ function toText(g) {
     return '!'
   } else if (g <= 240) {
     return '8'
-  } else return '&nbsp'
-}
-
-//更改图片名称
-function imgName(length) {
-  if (length < 9) return '0000' + (length + 1)
-  if (length < 99) return '000' + (length + 1)
-  if (length < 999) return '00' + (length + 1)
-  if (length < 9999) return '0' + (length + 1)
-}
-
-//播放
-let a = 0
-
-function textPlayer() {
-  let player = get('myAudio')
-  let textDiv = get('text')
-  player.play()//播放音频
-  textDiv.innerHTML = img_arr[a]
-  if (a < img_arr.length - 1) {
-    a++
-    //30帧
-    setTimeout(textPlayer, 30.33333)
-  }
+  } else return '&nbsp;'
 }
